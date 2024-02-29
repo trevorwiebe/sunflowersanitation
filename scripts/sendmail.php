6ef44251da5f69fbd84cb7174508821f6b08caf5
trevor <thisistrevor4@gmail.com>
2024-02-29 10:21:38 -0600

    add send mail notice to main page.

--- scripts/sendmail.php
+++ scripts/sendmail.php
@@ -7,13 +7,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check that data was sent to the mailer.
     if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         // Set a 400 (bad request) response code and exit.
-        http_response_code(400);
-        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+        $responseCode = 400; // Bad Request
+        header("Location: http://sunflowersanitation.com?response=$responseCode");
         exit;
     }
 
     // Recipient email (replace with your email)
-    $recipient = "calvin@sunflowersanitation.com";
+    $recipient = "contact@sunflowersanitation.com";
 
     // Email subject
     $subject = "New contact from $name";
@@ -29,17 +29,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Send the email
     if (mail($recipient, $subject, $email_content, $email_headers)) {
         // Set a 200 (okay) response code.
-        http_response_code(200);
-        echo "Thank You! Your message has been sent.";
+        $responseCode = 200;
+        header("Location: http://sunflowersanitation.com?response=$responseCode");
     } else {
         // Set a 500 (internal server error) response code.
-        http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message.";
+        $responseCode = 500;
+        header("Location: http://sunflowersanitation.com?response=$responseCode");
     }
 
 } else {
     // Not a POST request, set a 403 (forbidden) response code.
-    http_response_code(403);
-    echo "There was a problem with your submission, please try again.";
+    $responseCode = 403;
+    header("Location: http://sunflowersanitation.com?response=$responseCode");
 }
 ?>
